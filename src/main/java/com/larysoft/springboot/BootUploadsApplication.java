cfb7831c4da3e15b509e034ef3ed7b18350fddf8
larysoft <63218281+larysoft@users.noreply.github.com>
2020-12-31 14:55:53 +0100

    src folder

---
+++ src/main/java/com/larysoft/springboot/BootUploadsApplication.java
@@ -0,0 +1,12 @@
+package com.larysoft.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootUploadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootUploadsApplication.class, args);
+    }
+}
